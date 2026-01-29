be34eefe515f1db2becd6faf705314816449263d
yadav04abhishek-design <yadav04.abhishek@gmail.com>
2026-01-29 23:40:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/PG-website/",   // 👈 EXACT repo name
   plugins: [react()],
   resolve: {
     alias: {
